69b0e65f8c848ee23d850fac335ec388c95f9ac8
Nic Bestauros <nbestauros@gmail.com>
2024-03-10 20:30:55 -0700

    Fixed fullscreen and text width expansion.

--- translate-it/translate_it/main.py
+++ translate-it/translate_it/main.py
@@ -1,23 +1,28 @@
 from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-# import WebDriverWait from selenium.webdriver.support.ui
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 
 def open_webdriver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
     options.add_argument("--kiosk")
+    # options.add_argument("--start-maximized")
     options.add_argument("--disable-infobars")
     options.add_argument("--force-dark-mode")
-    options.add_argument("--force-device-scale-factor=3")
+    options.add_argument("--force-device-scale-factor=4")
     options.add_argument("--hide-scrollbars")
     options.add_argument("--disable-extensions")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     driver = webdriver.Chrome(options=options)
+    # Moving right and up from primary display, adjust as necessary.
+    driver.set_window_position(300, -250)
+    sleep(1)
+    driver.maximize_window()
+    sleep(1)
 
     return driver
 
@@ -39,7 +44,7 @@ def open_website(driver):
     ).send_keys(code)
 
     driver.find_element(By.CSS_SELECTOR, "input[aria-label='Username']").send_keys(
-        "Timberlake"
+        "TimberlakeK"
     )
     driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
 
@@ -47,6 +52,8 @@ def open_website(driver):
 
     wait = WebDriverWait(driver, 10)
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, "messageList")))
+    # hits the f11 key to go fullscreen
+    driver.find_element(By.TAG_NAME, "body").send_keys(Keys.F11)
 
     script = """
     document.body.style.visibility = 'hidden';
@@ -54,7 +61,8 @@ def open_website(driver):
     var style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = '.userIconContainer { display: none !important; }';
-    style.innerHTML += '.messageContent { box-shadow: none; }';
+    style.innerHTML += '.messageContent { box-shadow: none; max-width: 100% !important;}';
+    style.innerHTML += '.messageContainer { max-width: 100% !important; padding-block: 0.5em !important;}';
     style.innerHTML += '.username { display: none !important; }';
     document.getElementsByTagName('head')[0].appendChild(style);
 
@@ -83,6 +91,7 @@ def open_website(driver):
     }
     """
     driver.execute_script(script)
+    driver.fullscreen_window()
 
     sleep(86400)
 
